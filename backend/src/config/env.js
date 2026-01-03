c66ee681781ae948a28288282bba22e383c6c10e
wisdom uzoma <princewizoma22@gmail.com>
2026-01-03 19:34:00 +0100

    new commit

---
+++ backend/src/config/env.js
@@ -0,0 +1,9 @@
+import deotenv from "dotenv ";
+
+deotenv.config();
+
+export const ENV = {
+  NODE_ENV: process.env.NODE_ENV,
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL  
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
 
  const app = express();
+
+ const _dirname = path.resolve();
 
  app.get("/api/health", (req, res) => {
     res.status(200).json({message: "Success"});
  });
 
- app.listen(3000, () => console.log("This server is Up and Running...")); 
+//  This will make the project ready for deploy ment
+if (ENV.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "--/admin/dist")))
+
+   app.get("/{*any}", (req,res) => {
+      res.sendFile(path.jason(__dirname, "../admin", "dist", "index.html"));
+   });
+}
+
+ app.listen(ENV, () => console.log("This server is Up and Running...")); 
